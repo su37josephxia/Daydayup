efee21c3f94bed6eb0ef8e9a9bbd1ca7e803a139
yaotou <632319345@qq.com>
2021-11-14 23:21:27 +0800

    Update index.js
    feat: template parse

--- src/code-template/index.js
+++ src/code-template/index.js
@@ -1,54 +1,34 @@
 /**
  * 模版拆分
  */
-const map = {
-  front: {
-    start: "// 前置代码 start",
-    end: "// 前置代码 end",
-    code: [],
-  },
-  template: {
-    start: "// 模板 start",
-    end: "// 模板 end",
-    code: [],
-  },
-  answer: {
-    start: "// 答案 start",
-    end: "// 答案 end",
-    code: [],
-  },
-  back: {
-    start: "// 后置代码 start",
-    end: "// 后置代码 end",
-    code: [],
-  },
-};
-function getTemplate(template) {
+ function getTemplate(template) {
   const templateArr = template.split("\n");
+  let stack = [] // 存放注释的栈，遇到end弹出
+    , codeArr = [] // 存放code数组的数组，如果遇到代码行，看stack的长度，始终操作stack.length - 1的code数组
+  let res = {}
+  let map = {
+    '// 前置代码 start': 'front',
+    '// 答案 start': 'answer',
+    '// 模板 start': 'template',
+    '// 后置代码 start': 'back',
+  }
   templateArr.forEach((item) => {
-    for (let key in map) {
-      if (item.indexOf(map[key].end) !== -1) {
-        map[key].active = false;
-      } else if (map[key].active) {
-        map[key].code.push(item);
-      } else if (item.indexOf(map[key].start) !== -1) {
-        map[key].active = true;
+    if (item.indexOf('// ') > -1) {
+      if (item.indexOf('start') > -1) { // 遇到start压栈
+        stack.push(item.trim())
+      } else if (item.indexOf('end') > -1){ // 遇到end组装res
+        const len = stack.length - 1
+        let key = map[stack.pop()]
+        res[key] = codeArr[len].join('\n')
+        codeArr.splice(len, 1)
       }
+    } else if (stack.length > 0) { // 其他时候当注释栈里有东西，code代表栈顶注释的代码
+      if (!codeArr[stack.length - 1]) codeArr[stack.length - 1] = []
+      codeArr[stack.length - 1].push(item)
     }
   });
-  map.template.code = map.template.code.filter((item) => {
-    const arr = map.answer.code.concat([map.answer.start, map.answer.end]);
-    const result = arr.find((item2) => {
-      return item.indexOf(item2) !== -1;
-    });
-    return !result;
-  });
-  return {
-    front: map.front.code.join("\n"),
-    template: map.template.code.join("\n"),
-    answer: map.answer.code.join("\n"),
-    back: map.back.code.join("\n"),
-  };
+  
+  return res
 }
 
 const result = getTemplate(`
