483882f5b59d0edc1a1fcaf60188fadbdee4a508
北斗 <1912128546@qq.com>
2021-11-15 01:11:30 +0800

    ✨ feat:ifelse解析模板字符串

--- src/code-template/index.beidou.js
+++ src/code-template/index.beidou.js
@@ -24,69 +24,74 @@ const map = {
   },
 }
 function getTemplate(template) {
-  //根据map中属性key创建状态值，key为true将代码放入map[key].code中
-  const actionMap = Object.keys(map).map((k) => {
-    const o = {}
-    o[k] = false
-    return o
-  })
+  const tagList = []
   //将代码字符串通过换行符分割为数组
   const stringArray = template.split('\n')
-
+  function getIndexByStart(s) {
+    for (let i = 0; i < codeList.length; i++) {
+      if (codeList[i].indexOf(s) > -1) {
+        return i
+      }
+    }
+    return -1
+  }
   //遍历分割后的数组
+  let codeList = []
+  const keyList = Object.keys(map)
   stringArray.forEach((string) => {
-    // 数组中字符串可能为空，可能是一段代码，可能是备注；
-    Object.keys(map).forEach((key) => {
-      // key为map中属性
-      const { start, end, code } = map[key]
-      //匹配到代码开始标识将状态值设置为true
-      if (string.indexOf(start) > -1) {
-        actionMap[key] = true
-        //return ,这句代码不要
-        return
-      } else if (string.indexOf(end) > -1) {
-        //匹配到代码开始标识将状态值设置为false
-        actionMap[key] = false
+    //if (!string) return
+    codeList.push(string)
+    for (let i = 0; i < keyList.length; i++) {
+      const key = keyList[i]
+      const len = codeList.length
+      const { end, start, code } = map[key]
+      if (string.indexOf(end) > -1) {
+        codeList.splice(len - 1, 1)
+        const idx = getIndexByStart(start)
+        const splice = codeList.splice(idx + 1, len)
+        const next = getIndexByStart('start')
+        if (next > -1) {
+          code.push(...codeList.slice(next + 1))
+        }
+        codeList.splice(idx, 1)
+        // 是否还有数据呢？
+        code.push(...splice)
       }
-      // 状态值为true,将代码放入code中
-      if (actionMap[key]) {
-        code.push(string)
-      }
-    })
+    }
   })
 
   const templateResult = {}
   Object.keys(map).map((key) => {
     templateResult[key] = map[key].code.join('\n')
   })
-
+  console.log('templateResult', templateResult.template)
   return templateResult
 }
 
-const result = getTemplate(`
-// 前置代码 start
-var input = [1,3,2]
-// 前置代码 end
-// 模板 start
-function sort(arr) {
-    // 答案 start
-    var len = arr.length;
-    for (var i = 0; i < len - 1; i++) {
-        for (var j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
-                var temp = arr[j+1];        // 元素交换
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-// 答案 end
-    return arr;
-}
-// 模板 end
-// 后置代码 start
-console.log(sort(input))
-// 后置代码 end
-    `)
-
-console.log(result)
+// const result = getTemplate(`
+// // 前置代码 start
+// var input = [1,3,2]
+// // 前置代码 end
+// // 模板 start
+// function sort(arr) {
+//     // 答案 start
+//     var len = arr.length;
+//     for (var i = 0; i < len - 1; i++) {
+//         for (var j = 0; j < len - 1 - i; j++) {
+//             if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+//                 var temp = arr[j+1];        // 元素交换
+//                 arr[j+1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+// // 答案 end
+//     return arr;
+// }
+// // 模板 end
+// // 后置代码 start
+// console.log(sort(input))
+// // 后置代码 end
+//     `)
+// console.log('result', result)
+module.exports = getTemplate
