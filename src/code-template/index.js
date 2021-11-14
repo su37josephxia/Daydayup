2d5b211529cbcfad63fd2e93be17ee6771a056db
allenl <liangle8090@gmail.com>
2021-11-14 15:04:07 +0800

    update:通过有限状态机的方式实现模板解析

--- src/code-template/index.js
+++ src/code-template/index.js
@@ -1,80 +1,115 @@
-/**
- * 模版拆分
- */
-const map = {
-  front: {
-    start: "// 前置代码 start",
-    end: "// 前置代码 end",
-    code: [],
-  },
-  template: {
-    start: "// 模板 start",
-    end: "// 模板 end",
-    code: [],
-  },
-  answer: {
-    start: "// 答案 start",
-    end: "// 答案 end",
-    code: [],
-  },
-  back: {
-    start: "// 后置代码 start",
-    end: "// 后置代码 end",
-    code: [],
-  },
-};
-function getTemplate(template) {
-  const templateArr = template.split("\n");
-  templateArr.forEach((item) => {
-    for (let key in map) {
-      if (item.indexOf(map[key].end) !== -1) {
-        map[key].active = false;
-      } else if (map[key].active) {
-        map[key].code.push(item);
-      } else if (item.indexOf(map[key].start) !== -1) {
-        map[key].active = true;
-      }
-    }
-  });
-  map.template.code = map.template.code.filter((item) => {
-    const arr = map.answer.code.concat([map.answer.start, map.answer.end]);
-    const result = arr.find((item2) => {
-      return item.indexOf(item2) !== -1;
-    });
-    return !result;
-  });
-  return {
-    front: map.front.code.join("\n"),
-    template: map.template.code.join("\n"),
-    answer: map.answer.code.join("\n"),
-    back: map.back.code.join("\n"),
-  };
-}
+module.exports = (template) => {
+    const FRONT_START = '// 前置代码 start'
+    const FRONT_END = '// 前置代码 end'
+    const TEMPLATE_START = '// 模板 start'
+    const TEMPLATE_END = '// 模板 end'
+    const ANSWER_START = '// 答案 start'
+    const ANSWER_END = '// 答案 end'
+    const BACK_START = '// 后置代码 start'
+    const BACK_END = '// 后置代码 end'
 
-const result = getTemplate(`
-// 前置代码 start
-var input = [1,3,2]
-// 前置代码 end
-// 模板 start
-function sort(arr) {
-    // 答案 start
-    var len = arr.length;
-    for (var i = 0; i < len - 1; i++) {
-        for (var j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
-                var temp = arr[j+1];        // 元素交换
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
+    let front = back = template = answer = ''
+    let currentToken = ''
+
+    parse(code)
+
+    //解析代码
+    function parse(code) {
+        let state = frontStart;
+        const arr = code.split('\n')
+
+        arr.forEach(line => {
+            state = state(line.trim());
+        })
+    }
+
+    //前置开始
+    function frontStart(string) {
+        if (string === FRONT_END) {
+            return frontEnd()
+        } else if (string === FRONT_START) {
+            return frontStart
+        } else {
+            currentToken += string
+            return frontStart
         }
     }
-// 答案 end
-    return arr;
-}
-// 模板 end
-// 后置代码 start
-console.log(sort(input))
-// 后置代码 end
-    `);
 
-console.log(result);
+    //前置结束
+    function frontEnd() {
+        front = currentToken
+        currentToken = ''
+        return templateStart
+    }
+
+    //模板开始
+    function templateStart(string) {
+        if (string === TEMPLATE_END) {
+            return templateEnd()
+        } else if (string === TEMPLATE_START) {
+            return templateStart
+        } else if (string === ANSWER_START) {
+            template = currentToken
+            currentToken = ''
+            return answerStart
+        } else {
+            currentToken += string
+            return templateStart
+        }
+    }
+
+    //模板结束
+    function templateEnd() {
+        template += currentToken //中间会有答案，所以模板是由前后两部分组成，这里要用+
+        currentToken = ''
+        return backStart
+    }
+
+    //回答开始
+    function answerStart(string) {
+        if (string === ANSWER_END) {
+            return answerEnd()
+        } else if (string === ANSWER_START) {
+            return answerStart
+        } else {
+            currentToken += string
+            return answerStart
+        }
+    }
+
+    //回答结束
+    function answerEnd() {
+        answer = currentToken
+        currentToken = ''
+        return templateStart
+    }
+
+    //后置开始
+    function backStart(string) {
+        if (string === BACK_END) {
+            return backEnd()
+        } else if (string === BACK_START) {
+            return backStart
+        } else {
+            currentToken += string
+            return backStart
+        }
+    }
+
+    //后置结束
+    function backEnd() {
+        back = currentToken
+        currentToken = ''
+        return End
+    }
+
+    //结束
+    function End() {}
+
+    return {
+        front,
+        template,
+        answer,
+        back
+    }
+};
