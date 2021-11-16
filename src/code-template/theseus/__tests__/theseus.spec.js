f030a08a850dfcda5afb72a1afe5bca51dd965f3
605572714 <605572714@qq.com>
2021-11-16 10:41:55 +0800

    测试用例
    测试用例

--- src/code-template/theseus/__tests__/theseus.spec.js
+++ src/code-template/theseus/__tests__/theseus.spec.js
@@ -1,6 +1,6 @@
-// describe("代码模板分离", () => {
-//   test('正常系', () => {
-const code = `
+describe("代码模板分离", () => {
+  test('正常系', () => {
+    const code = `
     // 前置代码 start
     var input = [1,3,2]
     // 前置代码 end
@@ -26,20 +26,30 @@ const code = `
     // 后置代码 end
         `;
 
-const parse = require('../theseus')
-const {
-  front,
-  template,
-  answer,
-  back
-} = parse(code)
-console.log(
-  'front=>' + front + '\n',
-  'template=>' + template + '\n',
-  'answer=>' + answer + '\n',
-  'back=>' + back + '\n'
-);
-//   });
+    const parse = require('../theseus')
+    const {
+      front,
+      answer,
+      template,
+      back
+    } = parse(code)
 
-//   // TODO: 异常系 所有违反规定的依赖抛error
-// });
+    expect(front).toBe(`    var input = [1,3,2]`)
+    expect(answer).toBe(`        var len = arr.length;
+            for (var i = 0; i < len - 1; i++) {
+                for (var j = 0; j < len - 1 - i; j++) {
+                    if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                        var temp = arr[j+1];        // 元素交换
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }`)
+    expect(template).toBe(`    function sort(arr) {
+            return arr;
+        }`)
+    expect(back).toBe(`    console.log(sort(input))`)
+  });
+
+  // TODO: 异常系 所有违反规定的依赖抛error
+});
