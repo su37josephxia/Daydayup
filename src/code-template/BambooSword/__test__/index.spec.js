1e636102ae1add9fbe670d2113a5f18add6c2d7f
BambooSword <dengliming@tngdigital.com.my>
2021-11-16 12:54:13 +0800

    add the error test

--- src/code-template/BambooSword/__test__/index.spec.js
+++ src/code-template/BambooSword/__test__/index.spec.js
@@ -46,4 +46,35 @@ describe('代码模板分离', () => {
   })
 
   // TODO: 异常系 所有违反规定的依赖抛error
+  test('异常系', () => {
+    const code = `
+    // 前置代码 start
+    var input = [1,3,2]
+    // 前置代码 end
+    // 模板 start
+    function sort(arr) {
+        // 答案 start
+        var len = arr.length;
+        for (var i = 0; i < len - 1; i++) {
+            for (var j = 0; j < len - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                    var temp = arr[j+1];        // 元素交换
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        // 模板 end
+    // 答案 end
+        return arr;
+    }
+    // 后置代码 start
+    console.log(sort(input))
+    // 后置代码 end
+        `
+
+    const parse = require('../index')
+    const res = parse(code)
+    expect(res).toEqual(expect.stringContaining(`You got an error`))
+  })
 })
