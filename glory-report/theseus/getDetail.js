853e459e7950834dc4d7925bb854615293efe7a9
Theseus <605572714@qq.com>
2021-11-22 17:23:20 +0800

    获取bilibiliup主主页信息
    获取bilibiliup主主页信息，第一版

---
+++ glory-report/theseus/getDetail.js
@@ -0,0 +1,38 @@
+const request = require('request');
+const JSDOM = require('jsdom').JSDOM;
+const zlib = require('zlib');
+
+const url = 'https://www.bilibili.com/video/BV1VS4y1R7n6';
+
+exports.getDetail = function (url) {
+  return new Promise((resolve, reject) => {
+    request({
+      url, //请求路径
+      method: "GET", //请求方式，默认为get
+      headers: { //设置请求头
+        "Host": "www.bilibili.com",
+        "content-type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Cookie": "fingerprint=98528e38b2141f770da8a6e4e87201c2; buvid_fp=F29C57BE-7D95-F75C-0143-66F9947B38D804141infoc; buvid_fp_plain=F29C57BE-7D95-F75C-0143-66F9947B38D804141infoc; buvid3=FCBF455B-3ABE-42C3-A561-96F4BF590C10167631infoc; CURRENT_FNVAL=976; _uuid=9ADE95C5-0C2B-A367-4715-B03D0F627B4C04636infoc; blackside_state=1; sid=ky39dmmk; rpdid=|(J~kk|RRu)k0J'uYk~mYmu|Y; fingerprint=6eb96a833466e0fb2958fe834f633ce1; buvid_fp=F29C57BE-7D95-F75C-0143-66F9947B38D804141infoc; buvid_fp_plain=F29C57BE-7D95-F75C-0143-66F9947B38D804141infoc; SESSDATA=e0864ee5%2C1643521197%2C860dc%2A81; bili_jct=210c477fb4f8939d259c1da159b2d914; DedeUserID=237295541; DedeUserID__ckMd5=c8af4f7e0e0b2c42; PVID=1; CURRENT_QUALITY=0; LIVE_BUVID=AUTO6416342675647337; video_page_version=v_old_home_20; fingerprint3=bf2aa0464d5d9137c696f4955b11c534; fingerprint_s=bf902a5168710642d5638d1e6ced4629; bp_t_offset_237295541=594641391821987193; bp_video_offset_237295541=594643543602708166; innersign=1; bsource=share_source_weixinchat",
+      },
+      encoding: null
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        zlib.gunzip(body, function (err, decoded) {
+          if (decoded) {
+            const detail = new JSDOM(decoded.toString(), {
+              runScripts: "dangerously"
+            }).window.__INITIAL_STATE__.videoData.stat;
+            resolve(detail)
+          } else {
+            resolve({})
+          }
+        })
+      } else {
+        console.log(response.statusCode, error);
+      }
+    });
+  })
+}
+
+// getDetail("https://www.bilibili.com/video/BV1dh411p7DL")
--- glory-report/theseus/index.js
+++ glory-report/theseus/index.js
@@ -1,54 +1,59 @@
-const https = require('https');
-const request = require('request');
-const JSDOM = require('jsdom').JSDOM;
-const zlib = require('zlib');
+const path = require('path');
+const fs = require('fs')
 
-let biliUrl = "https://api.bilibili.com/x/space/arc/search?mid=1334519689&pn=1&ps=25&index=1&jsonp=jsonp";
+let {
+  getList
+} = require('./getList.js')
+let {
+  getDetail
+} = require('./getDetail.js');
 
-https.get(biliUrl, (res) => {
-  let data = ''; //接口数据
-
-  res.on('data', (chunk) => {
-    data += chunk; //拼接数据块
-  });
-  res.on('end', function () {
-    let json = JSON.parse(data); //解析json
-    // console.log(json.data.list.vlist.length); //打印json
-    setData(json.data.list.vlist)
-  })
-}).on('error', () =>
-  console.log('获取数据出错!')
-);
-
-function setData(list) {
-  let data = {
-    list: []
-  };
-  if (list.length > 0) {
-    list.forEach(item => {
-      data.list.push({
-        title: item.title,
-        bvid: item.bvid
+let detail = {}
+let list = [];
+let len = 1
+async function get(url) {
+  getList(url).then(async res => {
+    let l = 1
+    for await (const item of res) {
+      await getDetail(item.video).then(res1 => {
+        if (res1) {
+          item.coin = res1.coin
+          item.like = res1.like
+          item.view = res1.view;
+          item.favorite = res1.favorite;
+          item.id = len
+          console.log(len);
+          len++
+        }
       })
-    });
-  }
-  console.log(data);
+    }
+    detail = {
+      author: res[0].author,
+      list: res,
+      total: res.length
+    }
+    createPage(detail)
+  })
 }
 
-// request({
-//   url: `https://www.bilibili.com/video/BV1Vf4y1K7im`, //请求路径
-//   method: "GET", //请求方式，默认为get
-//   headers: { //设置请求头
-//     "content-type": "application/json",
-//     "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-//   },
-// }, function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     zlib.gunzip(body, function (err, decoded) {
-//       const start = new JSDOM(decoded.toString(), {
-//         runScripts: "dangerously"
-//       }).window.__INITIAL_STATE__.videoData.stat;
-//       console.log(start);
-//     })
-//   }
-// });
+function createPage(detail) {
+  // // 要写入的json数据
+
+  // // 格式化json
+  let text = JSON.stringify(detail, null, '\t')
+  // 指定要创建的目录和文件名称 __dirname为执行当前js文件的目录
+  let file = path.join(__dirname, 'test.json')
+  //写入文件
+  fs.writeFile(file, text, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('文件创建成功' + file);
+    }
+  })
+}
+
+
+let BiLiUrl = "https://space.bilibili.com/353539995";
+// 输入所需要查找的UP主主页地址
+get(BiLiUrl)
