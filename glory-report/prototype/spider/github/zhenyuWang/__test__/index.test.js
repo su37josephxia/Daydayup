a4113b53f0db0681325edfb33933dac642e73aa3
zhenyuWang <13641039885@163.com>
2021-11-25 01:18:01 +0800

    feat: 爬取GitHub首页掘金文章及B站视频链接

---
+++ glory-report/prototype/spider/github/zhenyuWang/__test__/index.test.js
@@ -0,0 +1,20 @@
+const getData = require("../index.js");
+describe("获取github首页掘金文章及B站视频链接", () => {
+  test('正常系',() => {
+    const url = 'https://github.com/zhenyuWang/frontend-studybook';
+    return getData(url).then(res => {
+      const {articles,videos} = res;
+      // 判断返回值类型
+      expect(Array.isArray(articles)).toBe(true)
+      expect(Array.isArray(videos)).toBe(true)
+      console.log(articles)
+      // 判断数组中链接是否正确
+      if(articles.length){
+        expect(articles[0].startsWith('https://juejin.cn/post')).toBe(true)
+      }
+      if(videos.length){
+        expect(videos[0].startsWith('https://www.bilibili.com/video')).toBe(true)
+      }
+    })
+  })
+})
