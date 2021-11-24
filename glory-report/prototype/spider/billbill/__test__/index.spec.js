63b84dfe91a6e1762c8e8d051c117141010452c5
Theseus <605572714@qq.com>
2021-11-24 14:59:24 +0800

    获取bilibili第二版
    获取bilibili第二版
    获取UP主所有视频信息（作者，标题，描述，头图，视频地址，点赞投币收藏浏览量）
    获取UP主某一视频的点赞投币收藏浏览量
    生成json文件

--- glory-report/prototype/spider/billbill/__test__/index.spec.js
+++ glory-report/prototype/spider/billbill/__test__/index.spec.js
@@ -1,8 +1,20 @@
-const path = require('path')
 describe("正常系", () => {
-  test('视频页数据读取', async () => {
-    const {getDetail} = require('../getDetail')
-    const data = await getDetail('https://www.bilibili.com/video/BV1VS4y1R7n6')
-    console.log('data',data)
+  test('获取UP主所有视频信息', async () => {
+    const getUpDetail = require('../index')
+    const data = await getUpDetail('https://space.bilibili.com/1334519689')
+    console.log('data', data)
+    expect(typeof data == "object").toBe(true);
+  }, 10000)
+  test('获取视频点赞，浏览，投币，收藏', async () => {
+    const getDetail = require('../getDetail')
+    const detail = await getDetail('https://www.bilibili.com/video/BV1Yq4y1g7vQ')
+    console.log('coin', detail.coin)
+    console.log('like', detail.like)
+    console.log('collect', detail.collect)
+    console.log('view', detail.view)
+    expect(typeof detail.coin == "string" || 'number').toBe(true);
+    expect(typeof detail.like == "string" || "number").toBe(true);
+    expect(typeof detail.collect == "string" || "number").toBe(true);
+    expect(typeof detail.view == "string" || "number").toBe(true);
   })
 });
