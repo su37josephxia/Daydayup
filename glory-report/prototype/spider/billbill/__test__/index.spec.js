63839aded272f7a2944e5014c4087424c4663dfe
Theseus <605572714@qq.com>
2021-11-24 15:03:42 +0800

    1
    1

--- glory-report/prototype/spider/billbill/__test__/index.spec.js
+++ glory-report/prototype/spider/billbill/__test__/index.spec.js
@@ -2,16 +2,16 @@ describe("正常系", () => {
   test('获取UP主所有视频信息', async () => {
     const getUpDetail = require('../index')
     const data = await getUpDetail('https://space.bilibili.com/1334519689')
-    console.log('data', data)
+    console.log('UP主信息', data)
     expect(typeof data == "object").toBe(true);
-  }, 10000)
+  }, 20000)
   test('获取视频点赞，浏览，投币，收藏', async () => {
     const getDetail = require('../getDetail')
     const detail = await getDetail('https://www.bilibili.com/video/BV1Yq4y1g7vQ')
-    console.log('coin', detail.coin)
-    console.log('like', detail.like)
-    console.log('collect', detail.collect)
-    console.log('view', detail.view)
+    console.log('投币：', detail.coin)
+    console.log('点赞：', detail.like)
+    console.log('收藏：', detail.collect)
+    console.log('播放量：', detail.view)
     expect(typeof detail.coin == "string" || 'number').toBe(true);
     expect(typeof detail.like == "string" || "number").toBe(true);
     expect(typeof detail.collect == "string" || "number").toBe(true);
